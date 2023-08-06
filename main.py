396d0c5154981631cbe768f1012b95d0edb21716
Michael Mosgöller <mosgoeller.michael@gmail.com>
2023-08-06 21:02:43 +0200

    file reading

---
+++ main.py
@@ -0,0 +1,20 @@
+from configFileReader import ConfigFileReader
+from base64 import b64encode
+import requests
+
+fileConfig = ConfigFileReader('config.json')
+fileConfig.readConfigFile()
+jsonData = fileConfig.fileContent
+
+if fileConfig.fileContent is not None:
+    print("endpoint to call: " + jsonData['baseEndpoint'] + jsonData['taskEndpoint'].format(jsonData['workspaceId'],jsonData['projectId']))
+    print("token to use: {}:api_token".format(jsonData['token']))
+
+api_token = "{}:api_token".format(jsonData['token'])
+taskEndpoint = jsonData['baseEndpoint'] + jsonData['taskEndpoint'].format(jsonData['workspaceId'],jsonData['projectId'])
+
+
+data = requests.get(taskEndpoint,
+                    headers={'content-type': 'application/json',
+                             'Authorization': 'Basic %s' % b64encode(b"bb1d364bc0b2eacb2b2455e7d67202e6:api_token").decode("ascii")})
+print(data.json())
