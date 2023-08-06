62367b3c7f6457ea41ad8880352d8254ac138c34
Ikoki13 <michi.mosgoeller@gmail.com>
2023-08-06 22:56:54 +0200

    update project and workspace id

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ fileConfig.readConfigFile()
 jsonData = fileConfig.fileContent
 
 if fileConfig.fileContent is not None:
-    print("endpoint to call: " + jsonData['baseEndpoint'] + jsonData['taskEndpoint'].format(jsonData['workspaceId'],jsonData['projectId']))
-    print("token to use: {}:api_token".format(jsonData['token']))
+    api_token = "{}:api_token".format(jsonData['token'])
+    taskEndpoint = jsonData['baseEndpoint'] + jsonData['taskEndpoint'].format(jsonData['workspaceId'],
+                                                                              jsonData['projectId'])
+    print("endpoint to call: " + taskEndpoint)
+    print("token to use: " + api_token)
 
-api_token = "{}:api_token".format(jsonData['token'])
-taskEndpoint = jsonData['baseEndpoint'] + jsonData['taskEndpoint'].format(jsonData['workspaceId'],jsonData['projectId'])
-
-
-data = requests.get(taskEndpoint,
+response = requests.get(taskEndpoint,
                     headers={'content-type': 'application/json',
                              'Authorization': 'Basic %s' % b64encode(b"bb1d364bc0b2eacb2b2455e7d67202e6:api_token").decode("ascii")})
-print(data.json())
+print(response)
