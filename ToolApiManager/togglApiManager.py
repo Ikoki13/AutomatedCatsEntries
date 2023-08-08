9351ef3702782ca049e7b7e577cc9bfab1507bed
Georg Treitz <georg.treitz@gmail.com>
2023-08-08 16:14:49 +0200

    adds token variable from config to the api call

--- ToolApiManager/togglApiManager.py
+++ ToolApiManager/togglApiManager.py
@@ -10,17 +10,16 @@ class TogglApiManager(BaseApiManager):
         super().__init__()
         self.workspaceId = int(workspaceId)
         self.projectId = int(projectId)
-        self.token = token
+        self.token = token + ":api_token"
 
     def readTasksForToday(self):
         print("fetching tasks from today")
         # TODO add today as parameter and not make it hard coded
-        # TODO make token variable from config
         # TODO filter current running taks (duration is negative)
         response = requests.get("https://api.track.toggl.com/api/v9/me/time_entries?start_date={}&end_date={}".format(
             "2023-08-07T00:00:00.00Z", "2023-08-07T23:59:59.99Z"),
                                 headers={'Authorization': 'Basic %s' % b64encode(
-                                    b"bb1d364bc0b2eacb2b2455e7d67202e6:api_token").decode("ascii")})
+                                    self.token.encode()).decode("ascii")})
         print("fetching successful")
         filteredTaskList = filter(lambda task: task['project_id'] == self.projectId, response.json())
         return filteredTaskList
