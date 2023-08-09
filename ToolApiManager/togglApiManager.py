240772b7ebf12017f0c94bc99f0bf7580ec34c0c
Ikoki13 <michi.mosgoeller@gmail.com>
2023-08-09 14:39:45 +0200

    added today as selection basis for tasks

--- ToolApiManager/togglApiManager.py
+++ ToolApiManager/togglApiManager.py
@@ -1,3 +1,4 @@
+import datetime
 from base64 import b64encode
 
 import requests
@@ -14,14 +15,14 @@ class TogglApiManager(BaseApiManager):
 
     def readTasksForToday(self):
         print("fetching tasks from today")
-        # TODO add today as parameter and not make it hard coded
+        startDatetime = str(datetime.date.today()) + "T00:00:00.00Z"
+        endDatetime = str(datetime.date.today()) + "T23:59:59.99Z"
         # TODO make token variable from config
-        # TODO filter current running taks (duration is negative)
-        response = requests.get("https://api.track.toggl.com/api/v9/me/time_entries?start_date={}&end_date={}".format(
-            "2023-08-07T00:00:00.00Z", "2023-08-07T23:59:59.99Z"),
+        response = requests.get("https://api.track.toggl.com/api/v9/me/time_entries?start_date={}&end_date={}".format(startDatetime, endDatetime),
                                 headers={'Authorization': 'Basic %s' % b64encode(
                                     b"bb1d364bc0b2eacb2b2455e7d67202e6:api_token").decode("ascii")})
         print("fetching successful")
+        # TODO filter current running taks (duration is negative)
         filteredTaskList = filter(lambda task: task['project_id'] == self.projectId, response.json())
         return filteredTaskList
 
