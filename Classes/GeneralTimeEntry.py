4fef31ae3cb7558c784b61fcd904d6c117984691
Ikoki13 <michi.mosgoeller@gmail.com>
2023-08-10 11:54:45 +0200

    add case insensitiveness to tag comparison

--- Classes/GeneralTimeEntry.py
+++ Classes/GeneralTimeEntry.py
@@ -12,19 +12,18 @@ class GeneralTimeEntry:
 
     def getCatFormat(self):
         result = self.description
+        lowerCaseTags = list(map(lambda t:t.lower(), self.tags))
 
-        # TODO Tag matching case insensitive machen
-        # TODO Stunden auf 2 Kommastellen und 15 Minuten Blöcke formattieren
-        if 'Termin' in self.tags:
+        if 'termin' in lowerCaseTags:
             result = str(float(self.duration/3600)) + "h Teilnahme Termin '" + result + "'"
-        elif 'Vorbereitung' in self.tags:
+        elif 'vorbereitung' in lowerCaseTags:
             result = "Vorbereitung '" + result + "'"
-        elif 'Nachbereitung' in self.tags:
+        elif 'nachbereitung' in lowerCaseTags:
             result = "Nachbereitung '" + result + "'"
-        elif 'Protokoll' in self.tags:
+        elif 'protokoll' in lowerCaseTags:
             result = "Erstellung Protokoll für '" + result + "'"
-        elif 'Entwicklung' in self.tags:
-            result = "Entwicklung von " + result
+        elif 'entwicklung' in lowerCaseTags:
+            result = "Entwicklung " + result
 
         return result
 
